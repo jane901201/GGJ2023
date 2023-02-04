645138a7169fb9f553876b3d0547c01af9f96857
jane901201 <75302011+jane901201@users.noreply.github.com>
2023-02-04 17:23:24 +0800

    Add GameOverUI.

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Text currentScoreText;
+    [SerializeField] private Text historyScoreText;
+
+    public void SetCurrentScore(int score)
     {
-        
+        currentScoreText.text = score.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHistoryScore(int historyScore)
     {
-        
+        historyScoreText.text = historyScore.ToString();
     }
 }
