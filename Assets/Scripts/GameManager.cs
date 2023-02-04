5e7956c25b52257a915cc9dab3ca6704a5975aa4
Yi Chen <ck1001099@gmail.com>
2023-02-04 20:28:29 +0800

    final alpha

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,5 +10,14 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         mainGameUI.SetRootText((int) Mathf.Abs(playerTransform.position.y));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainGameScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainGameScene2");
+        }
     }
 }
