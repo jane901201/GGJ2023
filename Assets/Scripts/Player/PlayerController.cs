f4664194fc9e76c06bf142133d0797e093a9a63c
jane901201 <75302011+jane901201@users.noreply.github.com>
2023-02-04 11:35:32 +0800

    Add test tilemap position script.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Tilemap m_Tilemap;
+    [SerializeField] private int m_TopTilemap = 4;
+    [SerializeField] private int m_ButtonTilemap = -4;
+    [SerializeField] private float shootCd = .5f;
+
+    private Vector3Int m_CurrentPlayerPosition;
+
+    public bool IsCd { get; private set; }
+
+
+    private void Start()
     {
-        
+        m_CurrentPlayerPosition = new Vector3Int(-9, 0, 0);
+        transform.position = m_Tilemap.GetCellCenterWorld(m_CurrentPlayerPosition);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveUp()
     {
-        
+        if (m_CurrentPlayerPosition.y < m_TopTilemap)
+        {
+            m_CurrentPlayerPosition = m_CurrentPlayerPosition + new Vector3Int(0, 1, 0);
+            transform.position = m_Tilemap.GetCellCenterWorld(m_CurrentPlayerPosition);
+        }
+    }
+
+    public void MoveDown()
+    {
+        if (m_CurrentPlayerPosition.y > m_ButtonTilemap)
+        {
+            m_CurrentPlayerPosition = m_CurrentPlayerPosition + new Vector3Int(0, -1, 0);
+            transform.position = m_Tilemap.GetCellCenterWorld(m_CurrentPlayerPosition);
+        }
+    }
+
+    private IEnumerator CoolDown()
+    {
+        IsCd = true;
+        yield return new WaitForSeconds(shootCd);
+        IsCd = false;
     }
 }
