13a8e7d8dcc77b6a42bddb13cddf3b9dd5d2698f
jane901201 <75302011+jane901201@users.noreply.github.com>
2023-02-04 20:29:41 +0800

    Add BackgroundTestScene.

--- Assets/Scripts/Misc/BackgroundController.cs
+++ Assets/Scripts/Misc/BackgroundController.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour
 {
     public GameObject[] backGrounds;
+    public Transform player;
+    public float speed = 1.0f;
 
     void Update()
     {
         foreach (var go in backGrounds)
         {
-            go.transform.Translate(new Vector3(0, 0.01f * Time.deltaTime, 0));
-            if (go.transform.position.y < -19)
+            go.transform.Translate(new Vector3(player.forward.x, player.forward.y, 0));
+            if (go.transform.position.y < player.position.y)
             {
-                go.transform.SetPositionAndRotation(new Vector3(37.8f, go.transform.position.x, 0), Quaternion.identity);
-
+                go.transform.SetPositionAndRotation(new Vector3(0, go.transform.position.y, 0), Quaternion.identity);
             }
+            //if (go.transform.position.x < player.position.x)
+            //{
+            //    go.transform.SetPositionAndRotation(new Vector3(go.transform.position.x, 0, 0), Quaternion.identity);
+            //}
         }
     }
 }
