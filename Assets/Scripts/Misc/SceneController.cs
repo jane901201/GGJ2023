3b334598d65018c239626d1cc3429b5016f57c62
Yi Chen <ck1001099@gmail.com>
2023-02-05 15:04:01 +0800

    fixed scene switching error

--- Assets/Scripts/Misc/SceneController.cs
+++ Assets/Scripts/Misc/SceneController.cs
@@ -1,13 +1,30 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    public DontDestroy dontDestroy;
+    public Animator TitleAnimator;
+
+    public void GoLevelScene()
+    {
+        DontDestroy.dontDestroy.Inject();
+        SceneManager.LoadScene("MainGameScene");
+    }
 
     public void GoLevelScene(SceneObjectSetting sceneObjectSetting)
     {
-        dontDestroy.Inject(sceneObjectSetting);
+        StartCoroutine(Init(sceneObjectSetting));
+    }
+
+    public IEnumerator Init(SceneObjectSetting sceneObjectSetting)
+    {
+        TitleAnimator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1f);
+
+        DontDestroy.dontDestroy.Inject(sceneObjectSetting);
         SceneManager.LoadScene("MainGameScene");
     }
 
