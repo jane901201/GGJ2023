33b9f9ecea6569d44f736240f462f51971b77cfb
Yi Chen <ck1001099@gmail.com>
2023-02-09 22:40:43 +0800

    added HideBtn for GameOverUI

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -6,8 +6,20 @@ public class GameOverUI : MonoBehaviour
     [SerializeField] private Text currentScoreText;
     [SerializeField] private Text historyScoreText;
 
+    [SerializeField] private Button _showBtn;
+    [SerializeField] private Button _hideBtn;
+    [SerializeField] private GameObject _mainUI;
+
     private const string CURRENT_SCORE_TEXT = "Score: ";
     private const string HISTORY_HIGHEST_SCORE_TEXT = "Highest Score: ";
+
+    public void Awake()
+    {
+        _showBtn.onClick.RemoveAllListeners();
+        _showBtn.onClick.AddListener(_Show);
+        _hideBtn.onClick.RemoveAllListeners();
+        _hideBtn.onClick.AddListener(_Hide);
+    }
 
     public void SetCurrentScore(int score)
     {
@@ -17,5 +29,15 @@ public class GameOverUI : MonoBehaviour
     public void SetHistoryScore(int historyScore)
     {
         historyScoreText.text = HISTORY_HIGHEST_SCORE_TEXT + historyScore.ToString();
+    }
+
+    private void _Show()
+    {
+        _mainUI.SetActive(true);
+    }
+
+    private void _Hide()
+    {
+        _mainUI.SetActive(false);
     }
 }
